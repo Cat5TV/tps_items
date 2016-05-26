d0f7772a5211357c6e093561064a94f8af9ef896
Robbie Ferguson <contact@category5.tv>
2016-05-26 11:09:35 -0400

    Add files via upload

---
+++ crafts.lua
@@ -0,0 +1,11 @@
+-- Various crafts
+
+--minetest.register_craft({
+--	output = "tps_items:pick_power",
+--	recipe = {
+--		{"gloopblocks:evil_block", "gloopblocks:evil_block"},
+--		{"gloopblocks:evil_block", "gloopblocks:evil_stick"},
+--		{"", "gloopblocks:evil_stick"},
+--	}
+--})
+
---
+++ init.lua
@@ -0,0 +1,64 @@
+--[[
+TPS Custom Items
+For The Pixel Shadow servers
+By Robbie Ferguson (My First Minetest Mod)
+--]]
+
+--[[
+ Great Resources:
+
+ https://github.com/minetest/minetest/blob/master/doc/lua_api.txt#L671
+ https://forum.minetest.net/viewtopic.php?id=3386
+
+ max usage is 65535, uses is what is takes from that number each time it's used... so higher the usage the quicker is degrades - TenPlus1
+ (65535/100) = USES
+ though this seems not to be the case.
+ 
+--]]
+
+
+-- Tools
+
+minetest.register_tool("tps_items:pick_founder", {
+	description = "TPS Founder Pickaxe",
+	inventory_image = "tps_items_power_pick.png",
+	tool_capabilities = {
+		full_punch_interval = 1.0,
+		stack_max=1,
+		range = 14.0,
+		groupcaps={
+			crumbly={times={[1]=0.3, [2]=0.2, [3]=0.1}, uses=1000, maxlevel=3},
+			cracky={times={[1]=1, [2]=0.6, [3]=0.2}, uses=500, maxlevel=3},
+			snappy={times={[1]=0.6, [2]=0.3, [3]=0.2}, uses=1000, maxlevel=3},
+			choppy={times={[1]=0.9, [2]=0.5, [3]=0.3}, uses=1000, maxlevel=3},
+			fleshy={times={[1]=4, [2]=3, [3]=2}, uses=200, maxlevel=3},
+			explody={times={[1]=3, [2]=3, [3]=3}, uses=10, maxlevel=3}
+		},
+		damage_groups = {fleshy=10},
+    		groups = {not_in_creative_inventory=1},
+	},
+})
+
+minetest.register_tool("tps_items:pick_power", {
+	description = "TPS Power Pickaxe",
+	inventory_image = "tps_items_power_pick.png",
+	tool_capabilities = {
+		full_punch_interval = 1.0,
+		stack_max=1,
+		range = 14.0,
+		groupcaps={
+			crumbly={times={[1]=0.3, [2]=0.2, [3]=0.1}, uses=5000, maxlevel=3},
+			cracky={times={[1]=1, [2]=0.6, [3]=0.2}, uses=500, maxlevel=3},
+			snappy={times={[1]=0.6, [2]=0.3, [3]=0.2}, uses=2000, maxlevel=3},
+			choppy={times={[1]=0.9, [2]=0.5, [3]=0.3}, uses=1000, maxlevel=3},
+			fleshy={times={[1]=4, [2]=3, [3]=2}, uses=200, maxlevel=3},
+			explody={times={[1]=3, [2]=3, [3]=3}, uses=10, maxlevel=3}
+		},
+		damage_groups = {fleshy=10},
+    		groups = {not_in_creative_inventory=1},
+	},
+})
+
+dofile(minetest.get_modpath("tps_items").."/crafts.lua")
+
+print("TPS Items Loaded!")
